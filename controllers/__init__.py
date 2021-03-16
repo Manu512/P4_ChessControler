d959c515541c2951013b59b774e346c27e7b3129
WALDNER Emmanuel <emmanuel.waldner@gmail.com>
2021-03-16 13:48:35 +0100

    __init__.py Flake8 control + DocString Ok Base Controller

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -3,8 +3,9 @@
 
 import re
 from typing import Union
-from views.views import Display
 from datetime import datetime as dt
+
+from views.views import Display
 
 
 class BaseController:
@@ -20,15 +21,15 @@ class BaseController:
 
     def ask_and_launch(self, ask_input="Votre choix : ", *, menu: dict):
         """
-        Methode pour interagir avec l'utilisateur, contrôler sa réponse
-        et définir la suite des évènements. En fonction des menus qui lui sont
-        présenté.
+        Method for interacting with the user, controlling their response
+        and define the sequence of events. According to the menus that are
+        presented.
 
         Methode Arguments :
         self : instance
-        ask_input : Texte qui sera demandé
-        menu : argument nommé qui contient un dictionnaire
-        dict{int(x):(tuple(Methode à lancer, Choix à afficher)}
+        ask_input : Text that will be asked
+        menu : named argument that contains a dictionary
+        dict{int(x):(tuple(Method to be launched, Choice to be displayed)}
         """
         user_input = input(ask_input)
 
@@ -39,97 +40,111 @@ class BaseController:
             except ValueError:
                 self.ask_and_launch(ask_input, menu=menu)
 
-            if self._check_choice(list(menu.keys()), user_input):
+            if self.check_choice(list(menu.keys()), user_input):
                 menu[user_input][0]()
             else:
                 self.ask_and_launch(ask_input, menu=menu_to_analyse)
 
-    def _check_choice(self, menu: list, response: int) -> bool:
+    def check_choice(self, menu: list, response: int) -> bool:
+        """
+        Control method for the choice entered by the user
+        Args:
+            menu: list
+            response: str : user entry
+
+        Returns: bool True if valid else False
+        """
         if response not in list(menu):
             self.view_menu.error_msg("Choix incorrect !! Veuillez ressaisir")
-            self.input_press_continue()
-            return False
+            ret = False
         else:
-            return True
+            ret = True
+        return ret
 
     def ask_and_store_text(self, ask_input='Votre saisie : ') -> tuple[bool, str]:
+        """
+        Validation method when entering text.
+        Args:
+            ask_input:
+
+        Returns: tuple(bool, user_entry)
+        """
         user_input = input(ask_input)
 
         if self._control_user_input('text', user_input):
-            return True, user_input
+            ret = True
         else:
-            return False, user_input
+            ret = False
+
+        return ret, user_input
 
     def ask_and_store_number(self, ask_input='Votre saisie : ') -> Union[tuple[bool, int], tuple[bool, str]]:
+        """
+        Validation method when entering number.
+        Args:
+            ask_input:
+
+        Returns: Union[tuple[bool, int], tuple[bool, str]]
+        tuple[bool, int] if valid input
+        tuple[bool, str] if invalid input
+        """
         user_input = input(ask_input)
 
         if self._control_user_input('number', user_input):
-            return True, int(user_input)
+            ret = True, int(user_input)
         else:
-            return False, user_input
+            ret = False, user_input
+
+        return ret
 
     # -------------------------CONTROL METHODS---------------------------------
 
     def _control_user_input(self, question, response) -> bool:
-        # Controle presence de chiffre dans le name/firstname
-        if question in ['name', 'first_name', 'text']:
+
+        if question in ['name', 'first_name', 'text']:  # Checking for numbers in name/firstname
             if self.__check_number_in_word(response):
                 self.view_menu.error_msg('Saisi non valide !\nVeuillez ressaisir !')
-                self.input_press_continue()
                 return False
-        if question == 'sentence':
+        elif question == 'sentence':    # Check if more than one word is present
             if self.__check_sentence(response) < 2:
                 self.view_menu.error_msg('Saisie non valide !\nVeuillez ressaisir !')
-                self.input_press_continue()
                 return False
-        elif question == 'dob':
-            # Controle le format de la date de naissance
+        elif question == 'dob':     # Check the date of birth
             try:
                 dt.strptime(response, '%d/%m/%Y')
             except ValueError:
                 self.view_menu.error_msg('Veuillez saisir une date de naissance'
                                          ' valide !\nVeuillez ressaisir !')
-                self.input_press_continue()
                 return False
-        elif question == '_genre':
+        elif question == '_genre':  # Checks whether the gender entry is correct.
             if response not in ['H', 'F']:
                 self.view_menu.error_msg('Genre saisi non valide !\nVeuillez ressaisir !')
-                self.input_press_continue()
                 return False
         elif question == 'bool':
-            if response not in ['O', 'N']:
+            if response not in ['O', 'N']:  # Checks whether the Y/N entry is correct.
                 self.view_menu.error_msg('Réponse non valide !\nVeuillez ressaisir !')
-                self.input_press_continue()
                 return False
-        elif question == 'number':
+        elif question == 'number':  # Checks the number entered when entering the Elo ranking
             try:
                 int(response)
             except ValueError:
                 self.view_menu.error_msg('Veuillez saisir un nombre entier !\nVeuillez ressaisir !')
-                self.input_press_continue()
                 return False
             else:
                 if int(response) < 0:
-                    self.view_menu.error_msg('Veuillez saisir un nombre entier !'
+                    self.view_menu.error_msg('Veuillez saisir un nombre positif !'
                                              '\nVeuillez ressaisir !')
-                    self.input_press_continue()
                     return False
         return True
 
     @staticmethod
-    def input_press_continue():
-        input("Pressez une touche pour continuer...")
-
-    @staticmethod
-    def __check_number_in_word(string):
-        """ Verification d'absence de chiffre dans
-         la variable passé en paramètre """
+    def __check_number_in_word(string: str):
+        """ Verification of the absence of a number in the variable passed in parameter """
         chiffre_pattern = re.compile('[0-9]')
         return re.search(chiffre_pattern, string)
 
     @staticmethod
-    def __check_sentence(string):
-        """ Verification d'absence de chiffre dans
-         la variable passé en paramètre """
+    def __check_sentence(string: str) -> int:
+        """Counting the number of words in the argument variable"""
         sentence_pattern = re.compile('[a-z]+')
         return len([*re.finditer(sentence_pattern, string)])
